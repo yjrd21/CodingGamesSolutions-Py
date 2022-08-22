b16679b1fc8e5d84542fe593a8b56265dbc643f5
yjrd21 <66791547+yjrd21@users.noreply.github.com>
2022-08-22 18:07:26 +0800

    Add files via upload

---
+++ Shadows Of The Knight-Episode 1.py
@@ -0,0 +1,27 @@
+import sys
+import math
+w, h = [int(i) for i in input().split()] #w- width of building, h-height of building
+n = int(input())  # maximum number of turns before game over. This info is quite irrelavent to my code but wtv 
+x, y = [int(i) for i in input().split()] #x and y are batman's initial coordinates
+
+# Binary search concept. First step is to setup play area
+minx=miny = 0
+maxx = w
+maxy = h
+
+# game loop
+while True:
+    # the direction of the bombs from batman's current location: U, UR, R, DR, D, DL, L or UL
+    # Manipulate the search area using if and elif statements 
+    direction = input() 
+    if 'U' in direction:
+        maxy = y - 1
+    elif 'D' in direction:
+        miny = y + 1
+    if 'L' in direction:
+        maxx = x - 1
+    elif 'R' in direction:
+        minx = x + 1
+    x = (minx+maxx) // 2 #use floor division "//" instead of division "/" operator to obtain int instead of float 
+    y = (miny+maxy) // 2
+    print(x,y)
